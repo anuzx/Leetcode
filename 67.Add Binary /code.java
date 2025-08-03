3ba9c07587e0aea2a4a518dd63628099c4a8dcc5
Anuj <anujparasar4266@gmail.com>
2025-08-03 13:53:38 +0530

    Update code.java

--- 67.Add Binary /code.java
+++ 67.Add Binary /code.java
@@ -1,1 +1,42 @@
+import java.util.*;
+
+public class Solution {
+
+    public static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter binary string a:");
+        String a = sc.nextLine();
+
+        System.out.println("Enter binary string b:");
+        String b = sc.nextLine();
+
+        System.out.println("Sum = " + addBinary(a, b));
+    }
+}
 
