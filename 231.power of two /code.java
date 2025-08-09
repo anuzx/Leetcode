ebebf1b6a87cbd88eb12b92531d0095ddd036b50
Anuj <anujparasar4266@gmail.com>
2025-08-09 14:50:15 +0530

    Create code.java

---
+++ 231.power of two /code.java
@@ -0,0 +1,11 @@
+public class Solution{
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));  
+        System.out.println(isPowerOfTwo(16));  
+        System.out.println(isPowerOfTwo(3)); 
+    }
+}
